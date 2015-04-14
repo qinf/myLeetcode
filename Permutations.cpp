84e5f9e13c4b7e6d88707bfeab19a1535d78a230
JunZhong Qin <qinjunzhong@gmail.com>
2015-04-13 23:25:12 -0400

    add Permutations

---
+++ Permutations.cpp
@@ -0,0 +1,51 @@
+/*
+ * Given a collection of numbers, return all possible permutations.
+ *
+ * For example,
+ * [1,2,3] have the following permutations:
+ * [1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], and [3,2,1].
+ *
+ */
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using std::endl; using std::cout;
+using std::vector; using std::sort;
+
+class Solution {
+public:
+    vector<vector<int> > permute(vector<int> &num) {
+        sort(num.begin(), num.end());
+        vector<vector<int> > result;
+        vector<int> path;
+        dfs(num, path, result);
+        return result;
+    }
+private:
+    void dfs(const vector<int> &num, vector<int> &path, vector<vector<int> > &result) {
+        if (path.size() == num.size()) {
+            result.push_back(path);
+            return;
+        }
+
+        for (auto i : num) {
+            if (find(path.begin(), path.end(), i) != path.end())
+                continue;
+            path.push_back(i);
+            dfs(num, path, result);
+            path.pop_back();
+        }
+    }
+};
+int main() {
+    Solution s;
+    vector<int> num = {1, 2, 3};
+    vector<vector<int> > result = s.permute(num);
+    for (auto &v : result) {
+        for (auto i : v)
+            cout << i << " ";
+        cout <<endl;
+    }
+    return 0;
+}
